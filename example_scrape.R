0af3b68143355199276863912b287af690d27df9
Tomas Okal <47049523+tomasokal@users.noreply.github.com>
2020-12-01 11:30:01 -0600

    Create example_scrape.R

---
+++ example_scrape.R
@@ -0,0 +1,33 @@
+# Necessary packages
+library(data.table)
+library(rvest)
+library(xml2)
+
+# Website to scrape
+url <- "https://www.numberfire.com/nfl/fantasy/fantasy-football-projections"
+
+# Read in data
+page <- rvest::html_table(xml2::read_html(url)
+                          , header = TRUE
+                          , fill = TRUE)
+                          
+# Combine and reshape files
+projections1 <- data.table::data.table(page[[1]])
+projections2 <- data.table::data.table(page[[2]])
+projections_dfs <- cbind(projections1, projections2)
+colnames(projections_dfs) <- c("v1", "v2", "v3", "v4", "v5", "v6", "v7", "v8", "v9", "v10", "v11", "v12", "v13", "v14", "v15", "v16", "v17", "v18", "v19", "v20", "v21", "v22", "v23", "v24")
+projections_dfs <- projections_dfs[-1, ]
+projections_dfs <- projections_dfs[, gsub1 := gsub("\t", "", v1), by = v1][, gsub2 := gsub("\n", "_", gsub1), by = gsub1]
+projections_dfs <- projections_dfs[, c("v25", "v26", "v27") := tstrsplit(gsub2, "_", fixed = TRUE)]
+projections_dfs <- projections_dfs[, .(PLAYER = v25
+                                       , POSITION = substr(gsub(".*\\((.*)\\).*", "\\1", v27), 1, 2)
+                                       , TEAM = trimws(substr(gsub(".*\\((.*)\\).*", "\\1", v27), 4, nchar(gsub(".*\\((.*)\\).*", "\\1", v27))))
+                                       , POINTS_FD = as.numeric(gsub("[\\$,]","", v16))
+                                       , POINTS_DK = as.numeric(gsub("[\\$,]","", v19))
+                                       , POINTS_YH = as.numeric(gsub("[\\$,]","", v22))
+                                       , SALARY_FD = as.numeric(gsub("[\\$,]","", v17))
+                                       , SALARY_DK = as.numeric(gsub("[\\$,]","", v20))
+                                       , SALARY_YH = as.numeric(gsub("[\\$,]","", v23)))]
+                                       
+# Export data 
+data.table::fwrite(projections_dfs, "Output/example_scrape_output.csv")                                     
